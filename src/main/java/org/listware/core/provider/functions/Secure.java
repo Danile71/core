64c08b064ac99af8d40ecc59293b4ebb9857ddfc
danil_e71 <danile71@gmail.com>
2023-03-26 20:32:48 +0300

    feat: security

---
+++ src/main/java/org/listware/core/provider/functions/Secure.java
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2023 NJWS Inc.
+ */
+
+package org.listware.core.provider.functions;
+
+import org.apache.flink.statefun.sdk.Context;
+import org.apache.flink.statefun.sdk.StatefulFunction;
+
+public class Secure implements StatefulFunction {
+
+	@Override
+	public void invoke(Context context, Object input) {
+
+	}
+
+}
